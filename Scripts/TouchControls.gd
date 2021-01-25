e5c4289a671ab3762c8b3589b95bfcb600587aa5
Nderim Topalli <nderim.topalli@medikura.com>
2021-01-25 13:09:26 +0100

    adding touchscreen

--- Scripts/TouchControls.gd
+++ Scripts/TouchControls.gd
@@ -1,2 +1,28 @@
-extends Node2D
+extends CanvasLayer
+
+
+
+func _on_UpBtn_pressed():
+	Input.action_press("ui_up")
+
+func _on_UpBtn_released():
+	Input.action_release("ui_up")
+
+func _on_LeftBtn_pressed():
+	Input.action_press("ui_left")
+	
+func _on_LeftBtn_released():
+	Input.action_release("ui_left")
+
+
+func _on_RightBtn_pressed():
+	Input.action_press("ui_right")
+
+func _on_RightBtn_released():
+	Input.action_release("ui_right")
+
+
+
+
+
 
